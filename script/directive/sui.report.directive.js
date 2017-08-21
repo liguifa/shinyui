2f9beb05c9a701e2cc1b774ce4cadb6eee0eb2e1
liguifa <liguifa18840848462@outlook.com>
2017-08-21 11:04:23 +0800

    update ui

--- script/directive/sui.report.directive.js
+++ script/directive/sui.report.directive.js
@@ -129,8 +129,57 @@
 .directive("suiFan", function () {
     var vm = {
         template: "<div><svg class='sui-report sui-report-fan' width='{{vm.w}}px' height='{{vm.h}}px'>\
-                    <path fill='red' stroke='none' d='M150,150L250,150A100,100,0,0,0,220.71067811865476,79.28932188134526Z' opacity='1' fill-opacity='1' stroke-width='2.9994665980652173' style='-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 1; fill-opacity: 1;' transform='matrix(1,0,0,1,0,0)'></path>\
-                   </svg></div>"
+                    <path fill='{{point.c}}' stroke='none' d='{{point.d}}' opacity='1' fill-opacity='1' stroke-width='2.9994665980652173' style='-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 1; fill-opacity: 1;' transform='matrix(1,0,0,1,0,0)' ng-repeat='point in vm.points'></path>\
+                   </svg></div>",
+        build:function(points,r){
+            var center = "M"+r/2+","+r/2;
+            var frontX = 300;
+            var frontY = 0;
+            var frontWeight = 0;
+            var addX = true;
+            var addY = true;
+            var newPoints = [];
+            for(var i in points){
+                // var weight = r*points[i];
+                // var tempX = addX ? frontX+weight:frontX-weight;
+                // if(tempX>=r)
+                // {
+                //     addX = false;
+                //     tempX = r - (tempX - r);
+                // }
+                // if(tempX<=0)
+                // {
+                //     addX = true;
+                //     tempX = 0 - tempX;
+                // }
+                // var tempY = addY ? frontY+weight:frontY-weight;
+                // if(tempY>=r)
+                // {
+                //     addY = false;
+                //     tempY = r - (tempY - r);
+                // }
+                // if(tempY<=0)
+                // {
+                //     addY = true;
+                //     tempY = 0 - tempY;
+                // }
+                var weight = 360 * points[i];
+                var tempWeight = 90-weight-frontWeight;
+                var y = Math.sin(tempWeight* 0.017453293) * r/2;
+                tempY = r/2 - y;
+                var tempWeight = frontWeight + weight;
+                var x = Math.sin(tempWeight* 0.017453293) * r/2;
+                tempX = r/2 + x;
+                frontWeight = weight;
+                newPoints.push({d:center+"L"+frontX+","+frontY+"A"+r/2+","+r/2+",0,1,1,"+tempX+","+tempY,c:this.GenerateColor()});
+                frontX = tempX;
+                frontY = tempY;
+            }
+            return newPoints;
+        },
+        GenerateColor:function(){
+            return "#"+parseInt(Math.random()*1000000);
+        }
     }
     return {
         restrict: "E",
@@ -156,7 +205,7 @@
             };
 
             $scope.$watch("points", function (points) {
-                $scope.vm.points = points;
+                $scope.vm.points = vm.build(points,$scope.vm.w);
             });
 
             $scope.$watch("dotX", function (x) {
@@ -165,14 +214,17 @@
 
             $scope.$watch("dotY", function (y) {
                 $scope.vm.dotY = y;
+               
             });
 
             $scope.$watch("width", function (width) {
                 $scope.vm.w = width;
+                  $scope.vm.points = vm.build($scope.points,width);
             });
 
             $scope.$watch("height", function (height) {
                 $scope.vm.h = height;
+                  $scope.vm.points = vm.build($scope.points,height);
             });
 
             $scope.$watch("title", function (title) {
